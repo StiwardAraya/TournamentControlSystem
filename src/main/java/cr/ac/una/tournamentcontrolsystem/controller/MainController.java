807f8dd2dc834ea940c32ac4cb4b3e76dc4e08be
Stiward Araya C <StiwardArayaCalderon98@gmail.com>
2025-03-07 14:05:26 -0600

    Change(controller): background image change animation implemented

--- src/main/java/cr/ac/una/tournamentcontrolsystem/controller/MainController.java
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/controller/MainController.java
@@ -1,11 +1,18 @@
 package cr.ac.una.tournamentcontrolsystem.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * Controlador de menú principal
@@ -18,21 +25,61 @@ public class MainController extends Controller implements Initializable {
 
     @FXML
     private AnchorPane root;
+
     @FXML
-    private VBox mainContainer;
+    private ImageView imvMain;
+
+    private List<Image> imagenes;
+    private int indiceActualImagen = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        setWindow();
+        cargarImagenes();
+        iniciarCambioDeImagen();
     }
 
     @Override
     public void initialize() {
-        setWindow();
+    }
+
+    private void cargarImagenes() {
+        imagenes = new ArrayList<>();
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/futbol_bg_image.jpg")));
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/golf_bg_image.jpg")));
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/futbolam_bg_image.jpg")));
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/basket_bg_image.jpg")));
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/tennis_bg_image.jpg")));
+        imagenes.add(new Image(getClass().getResourceAsStream("/cr/ac/una/tournamentcontrolsystem/resources/img/volleyball_bg_image.jpg")));
+    }
+
+    private void iniciarCambioDeImagen() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3), event -> cambiarImagen()));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 
     private void setWindow() {
-        // TODO
+        imvMain.fitWidthProperty().bind(root.widthProperty());
+        imvMain.fitHeightProperty().bind(root.heightProperty());
+        imvMain.preserveRatioProperty().setValue(false);
+    }
+
+    private void cambiarImagen() {
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), imvMain);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+
+        fadeOut.setOnFinished(event -> {
+            indiceActualImagen = (indiceActualImagen + 1) % imagenes.size();
+            imvMain.setImage(imagenes.get(indiceActualImagen));
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(0.5), imvMain);
+            fadeIn.setFromValue(0.0);
+            fadeIn.setToValue(1.0);
+            fadeIn.play();
+        });
+
+        fadeOut.play();
     }
 
 }
