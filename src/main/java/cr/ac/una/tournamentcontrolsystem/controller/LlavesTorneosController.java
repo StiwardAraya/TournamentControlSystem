a22c876fee06d0ed7f05b9af6585a42488ffb50f
Stiward Araya C <StiwardArayaCalderon98@gmail.com>
2025-04-02 22:12:55 -0600

    Add(controller): controller class for tournament bracket window

---
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/controller/LlavesTorneosController.java
@@ -0,0 +1,18 @@
+package cr.ac.una.tournamentcontrolsystem.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class LlavesTorneosController extends Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+}
