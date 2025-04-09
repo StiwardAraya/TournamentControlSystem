1b02239936f8b9127a897c70f05854b9154fbf2c
Stiward Araya C <StiwardArayaCalderon98@gmail.com>
2025-04-09 17:56:47 -0600

    Add(service): new Registry class for tournament brackets

---
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/service/RegistroLlavesTorneos.java
@@ -0,0 +1,60 @@
+package cr.ac.una.tournamentcontrolsystem.service;
+
+import cr.ac.una.tournamentcontrolsystem.model.LlavesTorneo;
+import cr.ac.una.tournamentcontrolsystem.util.Respuesta;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class RegistroLlavesTorneos {
+
+    private List<LlavesTorneo> llavesTorneos;
+    private static RegistroLlavesTorneos INSTANCE;
+    static final Logger logger = Logger.getLogger(RegistroLlavesTorneos.class.getName());
+
+    private RegistroLlavesTorneos() {
+        Respuesta respuestaGetLlavesTorneos = getLlavesTorneos();
+        if (respuestaGetLlavesTorneos.getEstado()) {
+            llavesTorneos = (List<LlavesTorneo>) respuestaGetLlavesTorneos.getResultado("LlavesTorneos");
+        } else {
+            llavesTorneos = new ArrayList<>();
+        }
+    }
+
+    public static RegistroLlavesTorneos getInstance() {
+        if (INSTANCE == null) {
+            synchronized (RegistroLlavesTorneos.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new RegistroLlavesTorneos();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private Respuesta getLlavesTorneos() {
+        return GestorArchivo.getInstance().cargarLlavesTorneos();
+    }
+
+    public Respuesta buscarLlavesTorneo(int idTorneo) {
+        for (LlavesTorneo llavesT : llavesTorneos) {
+            if (llavesT.getIdTorneo() == idTorneo) {
+                return new Respuesta(true, "Llaves de torneo encontradas con exito", "Llaves cargadas", "llaves", llavesT);
+            }
+        }
+
+        return new Respuesta(false, "El Torneo no tiene llaves", "llaves no existentes");
+    }
+
+    public Respuesta guardarLlavesTorneo(LlavesTorneo LlavesTorneo) {
+        if (LlavesTorneo != null) {
+            llavesTorneos.add(LlavesTorneo);
+        }
+
+        if (GestorArchivo.getInstance().persistLlavesTorneos(llavesTorneos).getEstado()) {
+            return new Respuesta(true, "Llaves de torneo guardadas", null);
+        } else {
+            return new Respuesta(false, "No se pudieron almacenar las llaves del torneo", "error al guardar llaves");
+        }
+    }
+}
