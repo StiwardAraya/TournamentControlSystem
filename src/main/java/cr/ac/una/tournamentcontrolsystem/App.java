8fede7ea8c7bf0b3f26621d2395a64bb88c9f155
Stiward Araya C <StiwardArayaCalderon98@gmail.com>
2025-04-09 17:58:20 -0600

    Fix(App): JVM camera thread bug

--- src/main/java/cr/ac/una/tournamentcontrolsystem/App.java
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/App.java
@@ -3,6 +3,7 @@ package cr.ac.una.tournamentcontrolsystem;
 import cr.ac.una.tournamentcontrolsystem.util.FlowController;
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +13,12 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        stage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
         FlowController.getInstance().InitializeFlow(stage, null);
         FlowController.getInstance().goViewInWindow("MainView");
     }
