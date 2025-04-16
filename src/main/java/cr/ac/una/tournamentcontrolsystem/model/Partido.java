e50cca194dddaf2fd0774a1614a66f6ba433cdaa
amarks1224 <122139000+amarks1224@users.noreply.github.com>
2025-04-15 18:08:14 -0600

    Change(root): add Ganador as winner

--- src/main/java/cr/ac/una/tournamentcontrolsystem/model/Partido.java
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/model/Partido.java
@@ -11,14 +11,20 @@ public class Partido {
 
     private int idPartido;
     private Torneo torneo;
+    private Equipo ganador; // Nuevo atributo para el equipo ganador
 
     public Partido() {
-        this(0, new Torneo());
+        this(0, new Torneo(), null);
     }
 
     public Partido(int idPartido, Torneo torneo) {
+        this(idPartido, torneo, null);
+    }
+
+    public Partido(int idPartido, Torneo torneo, Equipo ganador) {
         this.idPartido = idPartido;
         this.torneo = torneo;
+        this.ganador = ganador;
     }
 
     // SETTERS
@@ -30,6 +36,10 @@ public class Partido {
         this.torneo = torneo;
     }
 
+    public void setGanador(Equipo ganador) {
+        this.ganador = ganador;
+    }
+
     // GETTERS
     public int getIdPartido() {
         return idPartido;
@@ -39,12 +49,17 @@ public class Partido {
         return torneo;
     }
 
+    public Equipo getGanador() {
+        return ganador;
+    }
+
     @Override
     public String toString() {
-        return "Partido{"
-                + "idPartido=" + idPartido
-                + ", torneo=" + torneo
-                + '}';
+        return "Partido{" +
+                "idPartido=" + idPartido +
+                ", torneo=" + torneo +
+                ", ganador=" + ganador +
+                '}';
     }
 
 }
