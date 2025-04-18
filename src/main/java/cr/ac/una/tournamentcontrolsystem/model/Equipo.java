cffe4cbb71b21ecf9974340ad8bd39b7740dbb16
amarks1224 <122139000+amarks1224@users.noreply.github.com>
2025-04-18 11:13:24 -0600

    Change(root): bugs to access puntosTotales

--- src/main/java/cr/ac/una/tournamentcontrolsystem/model/Equipo.java
+++ src/main/java/cr/ac/una/tournamentcontrolsystem/model/Equipo.java
@@ -1,25 +1,27 @@
 package cr.ac.una.tournamentcontrolsystem.model;
 
+import java.util.Objects;
+
 /**
  * Clase que representa un Equipo.
- * 
+ *
  * @author Stiward Araya C.
  * @author Angie Marks S.
  * @author Kevin Calderon Z.
  */
 public class Equipo {
-    
+
     private int id;
     private String nombre;
     private String fotoURL;
     private boolean enTorneoActivo;
-    private int puntosTotales;
+    public int puntosTotales;
     private Deporte deporte;
-    
-    public Equipo(){
+
+    public Equipo() {
         this(0, "", "", false, 0, new Deporte());
     }
-    
+
     public Equipo(int id, String nombre, String fotoURL, boolean enTorneoActivo, int puntosTotales, Deporte deporte) {
         this.id = id;
         this.nombre = nombre;
@@ -83,5 +85,21 @@ public class Equipo {
     public String toString() {
         return this.nombre;
     }
-    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Equipo otroEquipo = (Equipo) obj;
+        return this.id == otroEquipo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id); 
+    }
 }
